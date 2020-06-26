a7afff84df7de75ab4ff19a9ce1958f8f00c14d1
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2020-06-26 14:40:18 +0200

    font REALLY displayed in pdf ;)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
-  def sort_by_last_update(arr)
-    puts arr
-    result = arr.order("updated_at DESC")
-    result
+  def sort_by_last_update(list)
+    list.order("updated_at DESC")
   end
 end
