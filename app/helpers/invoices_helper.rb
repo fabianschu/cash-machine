ff27c05084ab92e5d852187ef65fb559c2038bf5
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2020-06-28 16:37:48 +0200

    refactors helpers

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,5 +1,5 @@
 module InvoicesHelper
-  def invoice_total(positions)
-    positions.to_ary.reduce(0) { | sum, position | sum + position_total(position)}
+  def invoice_total(invoice)
+    invoice.positions.to_ary.reduce(0) { | sum, position | sum + position_total(position)}
   end
 end
