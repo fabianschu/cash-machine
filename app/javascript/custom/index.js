29e7463b62d7c18712332b891c55f1b0104d5959
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2020-07-06 01:10:41 +0200

    comments out js file

--- app/javascript/custom/index.js
+++ app/javascript/custom/index.js
@@ -1,22 +1,22 @@
-document.addEventListener("click", (e) => {
-  console.log(e);
-  if (e.target.getAttribute("class") === "clickable-invoice") {
-    window.open(e.target.getAttribute("data-href"), "_self");
-  }
+// document.addEventListener("click", (e) => {
+//   console.log(e);
+//   if (e.target.getAttribute("class") === "clickable-invoice") {
+//     window.open(e.target.getAttribute("data-href"), "_self");
+//   }
 
-  if (e.target.id === "new-invoice") {
-    window.open(e.target.getAttribute("data-href"), "_self");
-  }
+//   if (e.target.id === "new-invoice") {
+//     window.open(e.target.getAttribute("data-href"), "_self");
+//   }
 
-  if (e.target.id === "edit-customer") {
-    window.open(e.target.getAttribute("data-href"), "_self");
-  }
+//   if (e.target.id === "edit-customer") {
+//     window.open(e.target.getAttribute("data-href"), "_self");
+//   }
 
-  if (e.target.getAttribute("class") === "clickable-customer") {
-    window.open(e.target.getAttribute("data-href"), "_self");
-  }
+//   if (e.target.getAttribute("class") === "clickable-customer") {
+//     window.open(e.target.getAttribute("data-href"), "_self");
+//   }
 
-  if (e.target.getAttribute("class") === "clickable-position") {
-    window.open(e.target.getAttribute("data-href"), "_self");
-  }
-});
+//   if (e.target.getAttribute("class") === "clickable-position") {
+//     window.open(e.target.getAttribute("data-href"), "_self");
+//   }
+// });
